3fdb90e4ac451c265a52b534bc594fef99fc4479
xieyezi <1435398529@qq.com>
2021-01-21 14:51:31 +0800

    fix: 修复eslint报错

--- src/store/modules/about/detail.ts
+++ src/store/modules/about/detail.ts
@@ -1,11 +1,9 @@
 export interface AboutDetailState {
 	title: string
-	content: string
 }
 
-const state: AboutDetailState = {
-	title: 'title from about detail state model.',
-	content: 'content from about detail state model.'
+const state = {
+	title: 'title from about detail state model.'
 }
 const getters = {}
 const mutations = {}
